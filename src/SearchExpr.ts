fa4c9b620ea701e8e3a8f3b969b3278c6213037c
CJ Bell <cj@csail.mit.edu>
2016-07-12 09:00:43 +0100

    initial commit

---
+++ src/SearchExpr.ts
@@ -0,0 +1,72 @@
+
+function regexpReverseExec(re: RegExp, text: string, start?: number) {
+  if(start === undefined)
+	  start = re.lastIndex;
+	else if(start < 0)
+	  start = 0;
+  // Make sure we do a global search
+  re = re.global
+		? re
+		: new RegExp(re.source, 'g' + re.flags); 
+
+	re.lastIndex = 0;
+	let lastMatch : RegExpExecArray = null;
+	let match : RegExpExecArray;
+	let idx = re.lastIndex;
+	while((match = re.exec(text)) != null && match.index < start && match[0] !== '') {
+		lastMatch = match;
+		re.lastIndex = match.index+1; // do not jump further than one character from the match
+	}
+	if(match!=null && match.length>0 && match[0] == '')
+		return null;
+	else
+		return lastMatch;
+}
+
+
+export class SearchExpr {
+	private re: RegExp;
+	private lastIdx : number;
+	constructor(private searchTerm: string, private multiline: boolean, private useRegExp: boolean, private caseSensitive: boolean) {
+		if(useRegExp)
+		  this.re = new RegExp(searchTerm,'g' + (caseSensitive ? '' : 'i' ) + (multiline ? 'm' : '' ))
+		this.lastIndex = 0;
+	}
+
+	public set lastIndex(value:number) {
+		if(this.useRegExp)
+			this.re.lastIndex = value;
+		else
+			this.lastIdx = value;
+	}
+
+	public get lastIndex() : number {
+		if(this.useRegExp)
+			return this.re.lastIndex;
+		else
+			return this.lastIdx;
+	}
+
+	public exec(text: string, reverse = false) : {index: number} & Array<string> {
+		if(this.useRegExp) {
+			if(reverse)
+				return regexpReverseExec(this.re,text);
+			else
+				return this.re.exec(text);
+		} else {
+			let idx;
+			if(reverse)
+				idx = text.lastIndexOf(this.searchTerm.substring(0,this.lastIndex+this.searchTerm.length-1), this.lastIdx);
+			else
+				idx = text.indexOf(this.searchTerm,this.lastIdx);
+
+			if(idx < 0)
+				return null;
+
+			let result = <{index: number} & Array<string>>{};
+			result[0] = this.searchTerm;
+			result.index = idx;
+			return result;
+		}
+	}
+}
---
+++ src/extension.ts
@@ -0,0 +1,238 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+import {SearchExpr} from './SearchExpr';
+
+let processingSearch : boolean = false;
+let reverseSearch = false;
+let searchTerm = '';
+let initialSelections : vscode.Selection[];
+let status : {title: vscode.StatusBarItem, matchCase: vscode.StatusBarItem, useRegExp: vscode.StatusBarItem};
+let caseSensitive = true;
+let useRegExp = true;
+let multiline = true;
+// Used to distinguish between our selections and the user directly changing the selection
+let intendedSelections : vscode.Selection[] = [];
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+	function registerTextEditorCommand(commandId:string, run:(editor:vscode.TextEditor,edit:vscode.TextEditorEdit,...args:any[])=>void): void {
+    context.subscriptions.push(vscode.commands.registerTextEditorCommand(commandId, run));
+  }
+  function registerCommand(commandId:string, run:(...args:any[])=>void): void {
+    context.subscriptions.push(vscode.commands.registerCommand(commandId, run));
+  }
+	vscode.commands.executeCommand('setContext', 'incrementalSearch', false);
+
+	status = {
+		title: vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left,10),
+		matchCase: vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left,9),
+		useRegExp: vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left,8),
+	}
+	context.subscriptions.push(status.title, status.matchCase, status.useRegExp);
+
+	registerTextEditorCommand('extension.incrementalSearch.forward', (editor: vscode.TextEditor) => {
+		if(!processingSearch) {
+			beginSearch(editor);
+		} else {
+			// Advance the search
+			advanceSearch();
+		}
+	});
+
+	registerTextEditorCommand('extension.incrementalSearch.reverse', (editor: vscode.TextEditor) => {
+		if(!processingSearch) {
+			beginSearch(editor,{reverse:true});
+		} else {
+			// Advance the search
+			advanceSearch({reverse: true});
+		}
+	});
+
+	registerTextEditorCommand('extension.incrementalSearch.expand', (editor: vscode.TextEditor) => {
+		if(processingSearch) {
+			advanceSearch({expand: true});
+		}
+	});
+
+	registerTextEditorCommand('extension.incrementalSearch.reverseExpand', (editor: vscode.TextEditor) => {
+		if(processingSearch) {
+			advanceSearch({reverse: true, expand: true});
+		}
+	});
+
+	registerTextEditorCommand('extension.incrementalSearch.toggleRegExp', (editor: vscode.TextEditor) => {
+		useRegExp = !useRegExp;
+		updateSearch();
+	});
+	registerTextEditorCommand('extension.incrementalSearch.toggleCaseSensitive', (editor: vscode.TextEditor) => {
+		caseSensitive = !caseSensitive;
+		updateSearch();
+	});
+
+	registerTextEditorCommand('extension.incrementalSearch.stop', (editor) => {
+		setEditorSelections(editor,initialSelections);
+		stopSearch();
+	});
+	vscode.window.onDidChangeActiveTextEditor(() => stopSearch());
+	vscode.window.onDidChangeTextEditorSelection(onSelectionsChanged);
+	vscode.workspace.onDidChangeTextDocument(() => stopSearch);
+	vscode.workspace.onDidCloseTextDocument(() => stopSearch);
+
+	registerCommand('extension.incrementalSearch.backspace', () => {
+		if(processingSearch) {
+			searchTerm = searchTerm.substr(0,searchTerm.length-1);
+			updateSearch();
+		}
+	});
+
+	registerCommand('type', (event: {text:string}) => {
+		if(processingSearch) {
+      searchTerm+= event.text;
+			updateSearch();
+		}
+		else
+			vscode.commands.executeCommand('default:type', event);
+	});
+
+
+
+}
+
+
+function showStatus() {
+	status.title.text = 'incremental search: ';
+	status.matchCase.text = 'Aa';
+	status.useRegExp.text = '.*';
+	status.matchCase.command = 'extension.incrementalSearch.toggleCaseSensitive';
+	status.useRegExp.command = 'extension.incrementalSearch.toggleRegExp';
+	status.matchCase.tooltip = 'Toggle case sensitivity.'
+	status.useRegExp.tooltip = 'Toggle the use of regular expressions for search.'
+	status.title.show();
+	status.matchCase.show();
+	status.useRegExp.show();
+}
+
+function hideStatus() {
+	status.title.hide();
+	status.matchCase.hide();
+	status.useRegExp.hide();
+}
+
+function updateStatus(options = {reverse: false}) {
+	status.title.text = 'incremental search: ' + searchTerm;
+	status.title.color = 'white';
+	status.matchCase.color = caseSensitive ? 'white' : 'red';
+	status.useRegExp.color = useRegExp ? 'white' : 'red';
+}
+
+async function stopSearch(forwardCommand = '', ...args: any[]) {
+	await vscode.commands.executeCommand('setContext', 'incrementalSearch', false);
+	processingSearch = false;
+	hideStatus();
+	if(processingSearch) {
+		console.log('stopping incremental search.');
+	}
+	if(forwardCommand)
+		vscode.commands.executeCommand(forwardCommand, args);
+}
+
+function beginSearch(editor: vscode.TextEditor, options = {reverse: false}) {
+	processingSearch = true;
+	reverseSearch = options.reverse;
+	searchTerm = '';
+	initialSelections = editor.selections;
+	updateStatus(options);
+	showStatus();
+	vscode.commands.executeCommand('setContext', 'incrementalSearch', true);
+}
+
+function advanceSearch(options: {reverse?:boolean, expand?:boolean} = {reverse:false}) {
+	reverseSearch = options.reverse;
+	const editor = vscode.window.activeTextEditor;
+	let nextSelections = [];
+	for(const sel of editor.selections) {
+		// nextSelections.push(new vscode.Selection(sel.active,sel.active));
+		if(options.reverse)
+		  nextSelections.push(new vscode.Selection(sel.start,sel.start));
+		else
+		  nextSelections.push(new vscode.Selection(sel.start.translate(0,1),sel.start.translate(0,1)));
+	}
+	initialSelections = nextSelections;
+	updateSearch(options);
+}
+
+function updateSearch(options : {expand?:boolean} = {expand:false}) {
+	if(!processingSearch)
+	  return;
+	try {
+		updateStatus();
+		const editor = vscode.window.activeTextEditor;
+		const text = editor.document.getText();
+		const search = new SearchExpr(searchTerm,multiline,useRegExp,caseSensitive);
+		let nextSelections = [];
+		if(options.expand)
+			nextSelections = editor.selections;
+
+		for(const sel of initialSelections) {
+			const start = editor.document.offsetAt(sel.active);
+			search.lastIndex = start;
+			const match = search.exec(text,reverseSearch);
+			if(match !== null) {
+				const newAnchor = editor.document.positionAt(match.index);
+				const newActive = editor.document.positionAt(match.index+match[0].length);
+				// if(reverseSearch)
+				// 	nextSelections.push(new vscode.Selection(newActive,newAnchor));
+				// else
+					nextSelections.push(new vscode.Selection(newAnchor, newActive));
+			}
+			// else
+			// 	nextSelections.push(sel);
+		}
+		if(nextSelections.length > 0)
+		  setEditorSelections(editor, nextSelections);
+		else {
+			status.title.color = 'yellow';
+			setEditorSelections(editor, initialSelections);
+		}
+	} catch(e) {
+		if(e instanceof SyntaxError) {
+			status.title.color = 'red';
+		}
+		else
+			console.error(e);
+	}
+}
+
+function normalizeSelections(selections: vscode.Selection[]) : vscode.Selection[] {
+  return selections
+		.sort((x,y) => x.start.isBefore(y.start) ? -1 : x.start.isAfter(y.start) ? 1 : x.end.isBefore(y.start) ? -1 : x.end.isAfter(y.end) ? 1 : 0)
+		.filter((sel,idx,sels) => idx == 0 ? true : !sel.isEqual(sels[idx-1]));
+}
+
+function setEditorSelections(editor:vscode.TextEditor, selections: vscode.Selection[]) {
+	if(selections.length == 0)
+		return;
+	intendedSelections = normalizeSelections(selections);
+	editor.selections = selections;	
+}
+
+
+/** Stops search if anyone else tries to modify the editor selections */
+function onSelectionsChanged(event:vscode.TextEditorSelectionChangeEvent) {
+  if(event.selections.length != intendedSelections.length)
+		stopSearch();
+	for(let idx =0; idx < intendedSelections.length; ++idx) {
+		if(!intendedSelections[idx].isEqual(event.selections[idx])) {
+			stopSearch();
+			return;
+		}
+
+	}
+}
+
+
+// this method is called when your extension is deactivated
+export function deactivate() {
+}
